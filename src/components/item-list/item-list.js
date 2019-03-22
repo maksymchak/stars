302108601085210d73afa93fd664f71c7741ea42
maksymchak <maksym4ak@gmail.com>
2019-03-22 15:41:51 +0200

    item-list add persons

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import './item-list.css';
+import SwapiService from "../../services/swapi-service";
+import Spinner from "../spinner/spinner";
 
 export default class ItemList extends Component {
 
@@ -11,13 +13,25 @@ export default class ItemList extends Component {
   };
 
   componentDidMount() {
-    this.SwapiService
+    this.swapiService
       .getAllPeople()
       .then((peopleList) => {
         this.setState({
           peopleList
         });
       });
+  }
+
+  renderItems(arr) {
+    return arr.map(({id, name}) => {
+      return (
+        <li className="list-group-item"
+            key={id}
+            onClick={() => this.props.onItemSelected(id)}>
+          {name}
+        </li>
+      );
+    });
   }
 
   render() {
@@ -28,17 +42,11 @@ export default class ItemList extends Component {
       return <Spinner />;
     }
 
+    const items = this.renderItems(peopleList);
+
     return (
       <ul className="item-list list-group">
-        <li className="list-group-item">
-          Luke Skywalker
-        </li>
-        <li className="list-group-item">
-          Darth Vader
-        </li>
-        <li className="list-group-item">
-          R2-D2
-        </li>
+        {items}
       </ul>
     );
   }
