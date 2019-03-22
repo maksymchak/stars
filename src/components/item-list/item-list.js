a1f749151b5ed985201a0cb7cace9d5d9f356f6b
maksymchak <maksym4ak@gmail.com>
2019-03-22 15:24:37 +0200

    swapi-service fix and item-list add spinner

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -4,7 +4,30 @@ import './item-list.css';
 
 export default class ItemList extends Component {
 
+  swapiService = new SwapiService();
+
+  state = {
+    peopleList: null
+  };
+
+  componentDidMount() {
+    this.SwapiService
+      .getAllPeople()
+      .then((peopleList) => {
+        this.setState({
+          peopleList
+        });
+      });
+  }
+
   render() {
+
+    const { peopleList } = this.state;
+
+    if (!peopleList) {
+      return <Spinner />;
+    }
+
     return (
       <ul className="item-list list-group">
         <li className="list-group-item">
