927efc488aae51e1a395752b938da39ecaf36b7e
maksymchak <maksym4ak@gmail.com>
2019-05-18 02:26:14 +0300

    Updates

---
+++ precache-manifest.db862730c65343fca9741a2316b0b18a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77cf242db7bde80d60cd",
+    "url": "/stars/static/css/main.cc597044.chunk.css"
+  },
+  {
+    "revision": "77cf242db7bde80d60cd",
+    "url": "/stars/static/js/main.3793b730.chunk.js"
+  },
+  {
+    "revision": "cf741dd048f5caccbb0c",
+    "url": "/stars/static/js/runtime~main.99e9745e.js"
+  },
+  {
+    "revision": "47b8b1a41abc66244fed",
+    "url": "/stars/static/js/2.5a56e569.chunk.js"
+  },
+  {
+    "revision": "23a8376ab094677d6d823da454a5eaa8",
+    "url": "/stars/index.html"
+  }
+];
